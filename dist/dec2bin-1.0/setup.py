f3589c0ef4e884a5edc79b3ef087033704dbc0ad
andrekorol <anrobits@yahoo.com.br>
2016-10-07 00:50:26 -0300

    New files

---
+++ dist/dec2bin-1.0/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(name='dec2bin',
+      version='1.0',
+      description='Turn decimal into binary',
+      author='Andre Rossi Korol',
+      author_email='anrobits@yahoo.com.br',
+      maintainer='Andre Rossi Korol',
+      maintainer_email='anrobits@yahoo.com.br',
+      url='https://github.com/andrekorol/dec2bin',
+      py_modules=['dec2bin'],
+      license='GNU GPLv3',
+      platforms='Linux, MacOS, Windows',
+      long_description='dec2bin is a Python function that takes an integer or floating '
+                       'point as parameter and returns the binary representation of that number.'
+      )
