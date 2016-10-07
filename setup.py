80dfb52bda9f47c4026663419651544461791d91
andrekorol <anrobits@yahoo.com.br>
2016-10-06 22:48:45 -0300

    Adding the setup.py file.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(name='dec2bin',
+      version='1.0',
+      description='Turn decimal into binary',
+      author='Andre Rossi Korol',
+      author_email='anrobits@yahoo.com.br',
+      maintainer='Andre Rossi Korol',
+      maintainer_email='anrobits@yahoo.com.br',
+      url='https://github.com/andrekorol/dec2bin',
+      py_modules=['dec2bin'],
+     )
